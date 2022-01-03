ee526506dd9d97f0e47814bb66c10b3c7af9304d
Paul Colomiets <paul@colomiets.name>
2022-01-04 01:10:18 +0200

    chumsky: some work on multi-line comments

--- lib/src/chumsky.rs
+++ lib/src/chumsky.rs
@@ -27,8 +27,8 @@ pub fn ws_char<E: Error<char>>() -> impl Parser<char, (), Error=E> {
     .ignored()
 }
 
-pub fn ws<E: Error<char>>() -> impl Parser<char, (), Error=E> {
-    ws_char().repeated().at_least(1).ignored()
+pub fn ws<E: Error<char> + 'static>() -> impl Parser<char, (), Error=E> {
+    ws_char().repeated().at_least(1).ignored().or(ml_comment())
 }
 
 pub fn comment<E: Error<char>>() -> impl Parser<char, (), Error=E> {
@@ -43,7 +43,7 @@ pub fn ml_comment<E: Error<char> + 'static>() -> impl Parser<char, (), Error=E>
             none_of('*').ignored(),
             just('*').then_ignore(none_of('/').rewind()).ignored(),
         )).repeated().ignored()
-        .delimited_by(just("/*"), just("*/"))
+        .delimited_by(just("/*"), just("*/")).ignored()
     })
 }
 
@@ -56,6 +56,7 @@ fn parser<S: Span>() -> impl Parser<char, Document<S>, Error=Simple<char>> {
 #[cfg(test)]
 mod test {
     use chumsky::prelude::*;
+    use chumsky::error::SimpleReason;
     use super::{ws, comment, ml_comment};
 
     fn parse<'x, P, T>(p: P, text: &'x str) -> Result<T, String>
@@ -65,7 +66,17 @@ mod test {
         if let Some(data) = data {
             return Ok(data);
         }
-        Err(errs.iter().map(|e| e.to_string()).collect::<Vec<_>>().join("\n"))
+        Err(errs.iter().map(|e| {
+            let kind = match e.reason() {
+                SimpleReason::Unexpected => "unexpected".into(),
+                SimpleReason::Unclosed { span, delimiter } => {
+                    format!("unclosed {} at {}..{}",
+                            delimiter, span.start(), span.end())
+                }
+                SimpleReason::Custom(text) => text.clone(),
+            };
+            format!("{}..{}: {}: {}", e.span().start(), e.span().end(), kind, e.to_string())
+        }).collect::<Vec<_>>().join("\n"))
     }
 
     #[test]
@@ -86,5 +97,29 @@ mod test {
         parse(ws().then(comment()), "   // hello").unwrap();
         parse(ws().then(comment()).then(ws()).then(comment()),
               "   // hello\n   //world").unwrap();
+    }
+
+    #[test]
+    fn parse_comment_err() {
+        let err = parse(ws(), r#"/* comment *"#).unwrap_err();
+        println!("{}", err);
+        assert_eq!(err,
+            "12..13: unexpected: found end of input but / was expected");
+
+        let err = parse(ws(), r#"/* comment"#).unwrap_err();
+        println!("{}", err);
+        // order of items is random
+        assert!(err.starts_with(
+                "10..11: unexpected: found end of input but one of"));
+
+        let err = parse(ws(), r#"/*/"#).unwrap_err();
+        println!("{}", err);
+        assert!(err.starts_with(
+                "3..4: unexpected: found end of input but one of"));
+
+        let err = parse(ws(), r#"xxx"#).unwrap_err();
+        println!("{}", err);
+        // nothing is expected for comment or whitespace
+        assert_eq!(err, "0..1: unexpected: found 'x' but / was expected");
     }
 }
